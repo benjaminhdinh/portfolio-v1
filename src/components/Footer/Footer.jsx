fd844502a662ea84e90c8fff3899cf9462b4db23
Benjamin Dinh <bhdinh@ucdavis.edu>
2020-06-22 19:10:23 -0700

    done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,11 +5,14 @@ import PortfolioContext from '../../context/context';
 const Footer = () => {
   const { footer } = useContext(PortfolioContext);
   const { networks } = footer;
+  const handleOnClick = () => {
+    window.scrollTo(0, 0)
+  }
 
   return (
     <footer className="footer navbar-static-bottom">
       <Container>
-        <a href='##top' aria-label="Back To Top" className="back-to-top">
+        <a onClick={handleOnClick} href='#top' aria-label="Back To Top" className="back-to-top">
           <i className="fa fa-angle-up fa-2x" aria-hidden="true" />
         </a>
         <div className="social-links">
