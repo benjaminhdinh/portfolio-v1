ad9f989c3b6cefda9993d553f14a3d9d3555d835
Benjamin Dinh <bhdinh@ucdavis.edu>
2020-06-22 18:46:56 -0700

    done

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -22,7 +22,7 @@ const Header = () => {
 
   return (
     <section id="hero" className="jumbotron">
-      <Container>
+      <Container id="top">
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
           <h1 className="hero-title">
             {title || ''}
