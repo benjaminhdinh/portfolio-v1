24e4122930a63d1c3dc36dbb594cdfdd26874198
Benjamin Dinh <bhdinh@ucdavis.edu>
2020-06-22 22:25:35 -0700

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -25,7 +25,7 @@ function App() {
   }, []);
 
   return (
-    <PortfolioProvider value={{ hero, about, projects, contact, footer }}>
+    <PortfolioProvider id = "top" value={{ hero, about, projects, contact, footer }}>
       <Hero />
       <About />
       <Projects />
