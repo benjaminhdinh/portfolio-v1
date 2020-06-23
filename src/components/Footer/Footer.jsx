d3781ab612a2bad86bec8934fb3717dfb45c0bdc
Benjamin Dinh <bhdinh@ucdavis.edu>
2020-06-22 18:32:41 -0700

    final

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,10 @@
 import React, { useContext } from 'react';
 import { Container } from 'react-bootstrap';
 import PortfolioContext from '../../context/context';
-import GithubButtons from '../GithubButtons/GithubButtons';
-
-import { githubButtons } from '../../mock/data';
-
 
 const Footer = () => {
   const { footer } = useContext(PortfolioContext);
   const { networks } = footer;
-  const { isEnabled } = githubButtons;
 
   return (
     <footer className="footer navbar-static-bottom">
@@ -38,7 +33,6 @@ const Footer = () => {
         <p className="footer__text">
           © {new Date().getFullYear()} Benjamin Dinh
         </p>
-        {isEnabled && <GithubButtons />}
       </Container>
     </footer>
   );
