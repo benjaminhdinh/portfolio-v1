6feb488cdf59ea8120a7a6d6ab6f30c74f125757
Benjamin Dinh <bhdinh@ucdavis.edu>
2020-06-22 18:57:35 -0700

    done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
   return (
     <footer className="footer navbar-static-bottom">
       <Container>
-        <a href='#hero' aria-label="Back To Top" className="back-to-top">
+        <a href='#top' aria-label="Back To Top" className="back-to-top">
           <i className="fa fa-angle-up fa-2x" aria-hidden="true" />
         </a>
         <div className="social-links">
