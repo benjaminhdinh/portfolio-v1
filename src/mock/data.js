45b1fbed32f63c50197ec1fb6ae3a5f3df0fd58b
Benjamin Dinh <bhdinh@ucdavis.edu>
2020-09-16 00:18:33 -0700

    font fix

--- src/mock/data.js
+++ src/mock/data.js
@@ -10,7 +10,7 @@ export const headData = {
 // HERO DATA
 export const heroData = {
   title: 'Hi, my name is ',
-  name: 'Benjamin',
+  name: 'Benjamin Dinh',
   subtitle: 'I am a software developer!',
   cta: 'Learn more about me',
 };
