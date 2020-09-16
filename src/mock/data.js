f8f42b84c2a3de70b5851f53208707a84f9bee0d
Benjamin Dinh <bhdinh@ucdavis.edu>
2020-09-15 23:42:25 -0700

    font

--- src/mock/data.js
+++ src/mock/data.js
@@ -30,7 +30,7 @@ export const projectsData = [
     id: uuidv4(),
     img: 'project4.jpg',
     title: 'AggieFeed App',
-    info: 'Built an Android app that accessed a REST API to render the university’s newsfeed - AggieFeed - where any element can be tapped on to show a more detailed view.',
+    info: 'Built an Android app that accessed a REST API to render the university’s newsfeed, AggieFeed, where any element can be tapped on to show a more detailed view.',
     info2: 'Overview: Kotlin, OkHTTP, GSON',
     url: 'https://drive.google.com/file/d/1vH078U3TXIbzWwnw4dqZkSJPtS7ijEUe/view?usp=sharing',
     repo: 'https://github.com/benjamin-dinh/IETAndroidAppProject', 
