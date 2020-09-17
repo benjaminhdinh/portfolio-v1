a7601c128c981949bc07bd132ed8fd2d4f402245
Benjamin Dinh <bhdinh@ucdavis.edu>
2020-09-17 14:53:21 -0700

    change font

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -37,7 +37,7 @@ const About = () => {
           <Col md={6} sm={12}>
             <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
               <div className="about-wrapper__info">
-                <p className="about-wrapper__info-text">
+                <p className="about-wrapper__info-text" style="font-family: 'Open Sans'">
                   {paragraphOne || ''}
                 </p>
                 <p className="about-wrapper__info-text">
